223dec649740c2240640ef8a17417877ed1a1c4d
Lee Ziming <lqm358730583@gmail.com>
2018-11-18 19:38:50 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int test = 0;
+        test = Calculation(1,2);
+        System.out.println("Calculation(1,2) = " + test);
+    }
+
+    private static int Calculation(int a, int b) {
+        int result = 0;
+        result = a + b;
+        return result;
+    }
+
+}
